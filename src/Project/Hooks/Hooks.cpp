1742cf57d7825682aa28529bc0c5630b348e89a2
Styyx <styyx228@gmail.com>
2023-03-05 23:14:51 +0100

    Updated Cmake presets thanks to po3
    also, now really the latest CommonLib-NG

--- src/Project/Hooks/Hooks.cpp
+++ src/Project/Hooks/Hooks.cpp
@@ -1,9 +1,9 @@
 #include "Hooks.h"
 #include "RE/Offsets.h"
-//thanks to Nightfallstorm for the AE offset. I don't know if the VR offset works at all
+
 void Loki::Hooks::InstallActorUpdateHook() {
 
-	REL::Relocation<std::uintptr_t> ActorUpdate{ RELOCATION_ID(36357, 37348), REL::VariantOffset(0x6D3, 0x674, 0x6D3) };
+	REL::Relocation<std::uintptr_t> ActorUpdate{ RELOCATION_ID(36357, 37348), REL::VariantOffset(0x6D3, 0x674, 0x6D3) }; //Credits for the AE Offset goes to Nightfallstorm. I don't know if the VR offset will work.
 
 	auto& trampoline = SKSE::GetTrampoline();
 	_ActorUpdateF = trampoline.write_call<5>(ActorUpdate.address(), ActorUpdateF);
